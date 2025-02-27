1372712872795dab5a4bc3b3cdd933627c827d7b
jijysun <jijysun@naver.com>
2025-02-27 16:56:07 +0900

    Update 1043_Lie.cpp
    Union Find 자료구조로 해결

--- BaekJoon/1043_Lie.cpp
+++ BaekJoon/1043_Lie.cpp
@@ -1,14 +1,81 @@
 #include <algorithm>
 #include <iostream>
-#include <sstream>
-#include <unordered_set>
 #include <vector>
 using namespace std;
 
-// https://www.acmicpc.net/problem/1043
+// https://www.acmicpc.net/problem/1043 // 완벽한 Union Find 문제.
 
-// 틀린 코드입니다!!
+int n, m, truth, number; // 사람 수, 파티 수, 진실 아는 사람, 입력용 번호 변수
+int parent[51]; // Union Find 집합
+vector<int> true_person; // 진실 아는 사람 벡터
+vector<vector<int>> party_people(50);
 
+int find_parents(int u) { // 부모를 찾는 함수, 경로 압축
+    if (u == parent[u]) return u;
+    return find_parents(parent[u]);
+}
+
+void merge(int u, int v) { // 노드 연결 함수. 합집합
+    u = find_parents(u), v = find_parents(v);
+    if (u == v) return;
+    parent[u] = v;
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cin >> n >> m >> truth;
+
+    for (int i = 0; i < truth; i++) { // 각 입력
+        cin >> number;
+        true_person.push_back(number);
+    }
+
+    for (int i = 1; i <= n; i++) { // 사람 수 만큼 부분집합 생성
+        parent[i] = i;
+    }
+
+    for (int i = 0; i < m; i++) { // 파티 수 만큼 반복
+        cin >> number; // 해당 파티의 인원 수 입력
+        int num, prev;
+
+        for (int j = 0; j < number; j++) {
+            if (j >= 1) { // 첫 원소 제외 모두는
+                prev = num;
+                cin >> num;
+                merge(prev, num); // 모두 같은 집합에 속하게끔
+            }
+            else {
+                cin >> num;
+            }
+            party_people[i].push_back(num);
+        }
+    }
+
+    for (vector<int>& party : party_people) { // 파티 만큼 반복
+        bool check = false;
+        for (int& person : party) { // 파티 내 인원 만큼 반복
+            if (check) {
+                break;
+            }
+            for (int& t : true_person) { // 진실 아는 사람 만큼 반복
+                if (find_parents(person) == find_parents(t)) { // 현 사람과 진실을 아는 사람의 부모가 같다면?
+                    check = true; // 몰랐던 사람도 진실을 알게 되므로 BREAK;
+                    break;
+                }
+            }
+        }
+        if (check) {
+            m--;
+        }
+    }
+    printf("%d\n", m);
+    return 0;
+}
+
+/*
+ *
+// 틀린 코드
 int n, m; // people, party
 
 string true_person_size; // 진실을 알고 있는 사람 수
@@ -35,13 +102,13 @@ void addTruth() {
     for (int i : truth) {
         printf("%d ", i);
     }
-    printf("\n-----\n");*/
+    printf("\n-----\n");#1#
 }
 
 void checkParty() {
     int party_count = m;
     for (int i = 0; i < party_people.size(); i++) {
-        for (int num : truth) {
+        for (int num: truth) {
             if (find(party_people[i].begin(), party_people[i].end(), num) != party_people[i].end()) {
                 // printf("cant go to %d party\n", i+1);
                 party_count--;
@@ -62,7 +129,7 @@ int main() {
 
     getline(cin, true_person_size);
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i<m; i++) {
         string s;
         getline(cin, s);
         party_num.push_back(s);
@@ -104,4 +171,4 @@ int main() {
     checkParty();
 
     return 0;
-}
+}*/
